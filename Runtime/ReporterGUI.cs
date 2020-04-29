369c8072a24929ac1b6866794f5761bd5a8c51b1
Sacristan <girts.kesteris@gmail.com>
2020-04-29 18:29:36 +0300

    migrate logs viewer to unity package

---
+++ Runtime/ReporterGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Sacristam.UnityLogsViewer.Runtime 
+{
+	public class ReporterGUI : MonoBehaviour
+	{
+		Reporter reporter;
+		void Awake()
+		{
+			reporter = gameObject.GetComponent<Reporter>();
+		}
+
+		void OnGUI()
+		{
+			reporter.OnGUIDraw();
+		}
+	}
+}
